b2a36432ef5c0eb6ed4c16c9ac0a7773a3b1814a
Vikas Gautam <vikasgautamug2003@gmail.com>
2025-08-05 15:33:24 +0530

    Vercel Added

--- db.js
+++ db.js
@@ -1,12 +1,14 @@
-// db.js
 const mongoose = require('mongoose');
+require('dotenv').config();
 
 const connectDB = async () => {
+  const mongoURI = process.env.MONGO_URI || 'mongodb://localhost:27017/vchess';
+
   try {
-    await mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost:27017/vchess');
+    await mongoose.connect(mongoURI);
     console.log('✅ MongoDB Connected');
   } catch (err) {
-    console.error('❌ MongoDB connection failed:', err);
+    console.error('❌ MongoDB connection failed:', err.message);
     process.exit(1);
   }
 };
